2fd7da695ae1a30b56e6d6d39f20f0ea5bb7182d
Maksim <necmakc@mail.ru>
2023-06-11 01:06:21 +0300

    Create array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо 
+// задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше 
+// обойтись исключительно массивами.
+
+//------functions------
+string[] CreateArray(int n)
+{
+    string[] stringArray = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Введите элемент {i + 1} из {n}: ");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+
+void ShowArray(string[] stringArray)
+{
+    for (int i = 0; i < stringArray.Length; i++)
+        Console.Write(stringArray[i] + " ");
+}
+
+// ------main------
+Console.Write("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = CreateArray(n);
+ShowArray(array);
