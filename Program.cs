e91c8e1ddaeebba6f2bb620d97fe251dc6524762
Maksim <necmakc@mail.ru>
2023-06-11 01:21:41 +0300

    finish!

--- Program.cs
+++ Program.cs
@@ -19,10 +19,31 @@ void ShowArray(string[] stringArray)
 {
     for (int i = 0; i < stringArray.Length; i++)
         Console.Write(stringArray[i] + " ");
+    Console.WriteLine();
+}
+
+string[] GetNewArray(string[] stringArray)
+{
+    int count = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+        if (stringArray[i].Length <= 3)
+            count++;
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++)
+        if (stringArray[i].Length <= 3)
+        {
+            newArray[j] = stringArray[i];
+            j++;
+        }
+    return newArray;
 }
 
 // ------main------
 Console.Write("Введите количество элементов в массиве: ");
 int n = Convert.ToInt32(Console.ReadLine());
 string[] array = CreateArray(n);
+Console.WriteLine("Исходный массив");
 ShowArray(array);
+Console.WriteLine("Новый массив");
+ShowArray(GetNewArray(array));
